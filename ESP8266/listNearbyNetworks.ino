128d2f6b8e6d6334f8173fdfcb7ec0f81158ede3
Marian Brinza <31028022+MarianBrinza@users.noreply.github.com>
2019-11-26 05:22:27 +0200

    Add files via upload

---
+++ ESP8266/listNearbyNetworks.ino
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  // disconnect any previous connections
+  WiFi.disconnect();
+  delay(1000);
+
+  //scan for nearby networks
+  int networks = WiFi.scanNetworks();
+  Serial.print("Nearby networks found: ");
+  Serial.println(networks);
+  Serial.println();
+  delay(500);
+
+  Serial.println("Networks: ");
+  for(int i=0; i < networks; i++){
+    Serial.print("     1: ");
+    Serial.println(WiFi.SSID(i));
+  }
+
+  Serial.println();
+  Serial.println();
+  Serial.println("Scan completed...");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
